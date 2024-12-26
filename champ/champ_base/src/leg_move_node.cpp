9ed46109f23113332d6b17b9ad1ebfd5b57f5aa0
chuckt601 <chucktaylorsan@gmail.com>
2024-12-26 21:15:57 +0000

    running basic leg move action server

--- champ/champ_base/src/leg_move_node.cpp
+++ champ/champ_base/src/leg_move_node.cpp
@@ -20,6 +20,7 @@ public:
         //publisher_ = this->create_publisher<my_package::msg::MyMessage>("custom_topic", 10);
         foot_pos_deltas_publisher_ = this->create_publisher<champ_msgs::msg::FootPosDeltas>("foot_pos_deltas", 10);
         CG_offset_publisher_ = this->create_publisher<std_msgs::msg::Float32MultiArray>("CG_offset", 10);
+        
         roll_subscriber_ = this->create_subscription<std_msgs::msg::Float32>(
         "roll", 10, std::bind(&leg_move_node::rollCallback_, this,  std::placeholders::_1));
         pitch_subscriber_ = this->create_subscription<std_msgs::msg::Float32>(
@@ -36,7 +37,15 @@ public:
             "balance_offset_action",
             std::bind(&leg_move_node::handle_goal, this, std::placeholders::_1, std::placeholders::_2),
             std::bind(&leg_move_node::handle_cancel, this, std::placeholders::_1),
-            std::bind(&leg_move_node::handle_accepted, this, std::placeholders::_1)
+            std::bind(&leg_move_node::handle_accepted_balance_offset, this, std::placeholders::_1)
+        );
+        
+        leg_move_server_ = rclcpp_action::create_server<Leg_move_actions>(
+            this,
+            "leg_move_action",
+            std::bind(&leg_move_node::handle_goal, this, std::placeholders::_1, std::placeholders::_2),
+            std::bind(&leg_move_node::handle_cancel, this, std::placeholders::_1),
+            std::bind(&leg_move_node::handle_accepted_leg_move, this, std::placeholders::_1)
         );
         
     }
@@ -57,11 +66,12 @@ private:
 
     
     rclcpp_action::Server<Leg_move_actions>::SharedPtr balance_offset_server_;
+    rclcpp_action::Server<Leg_move_actions>::SharedPtr leg_move_server_;
     
     rclcpp_action::GoalResponse handle_goal(
         const rclcpp_action::GoalUUID & uuid,
         std::shared_ptr<const Leg_move_actions::Goal> goal) {
-        RCLCPP_INFO(this->get_logger(), "Received goal request with target %f", goal->target);
+        RCLCPP_INFO(this->get_logger(), "Received goal request with target %f", goal->z_target);
         return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
     }
 
@@ -71,29 +81,25 @@ private:
         return rclcpp_action::CancelResponse::ACCEPT;
     }
 
-    void handle_accepted(const std::shared_ptr<GoalHandle_Leg_move_actions> goal_handle) {
+    void handle_accepted_balance_offset(const std::shared_ptr<GoalHandle_Leg_move_actions> goal_handle) {
         std::thread([this, goal_handle]() {
             auto goal = goal_handle->get_goal();
-            RCLCPP_INFO(this->get_logger(), "Executing goal with target %f", goal->target);
+            RCLCPP_INFO(this->get_logger(), "Executing goal with target %f", goal->z_target);
 
-            // Publish a custom message
-            //champ_base::msg::MyMessage message;
             
-            //message.data = "Action triggered";
-            //message.count = goal->target;
             std_msgs::msg::Float32MultiArray CG_offset;
             switch (goal->leg) {
                 case 0:
-                   CG_offset.data={goal->target,goal->target};
+                   CG_offset.data={-goal->z_target,-goal->z_target};
                    break;
                 case 1:
-                   CG_offset.data={goal->target,-goal->target};
+                   CG_offset.data={-goal->z_target,goal->z_target};
                    break;
                 case 2: 
-                   CG_offset.data={-goal->target,goal->target};
+                   CG_offset.data={goal->z_target,-goal->z_target};
                    break;
                 case 3:
-                   CG_offset.data={-goal->target,-goal->target};
+                   CG_offset.data={goal->z_target,goal->z_target};
                    break;
                 default:
                    CG_offset.data={0.0f, 0.0f};
@@ -125,6 +131,35 @@ private:
         }).detach();
     }
     
+    void handle_accepted_leg_move(const std::shared_ptr<GoalHandle_Leg_move_actions> goal_handle) {
+        std::thread([this, goal_handle]() {
+            auto goal = goal_handle->get_goal();
+            RCLCPP_INFO(this->get_logger(), "Executing goal with target %f", goal->z_target);
+ 
+            champ_msgs::msg::FootPosDeltas foot_pos_deltas;
+            for (int i = 0; i < 4; ++i) {
+              champ_msgs::msg::Transformation transform;
+              transform.translation.x = 0;
+              transform.translation.y = 0;
+              transform.translation.z = 0;
+
+              transform.rotation.x = 0;
+              transform.rotation.y = 0;
+              transform.rotation.w = 0;
+              // Assign the transform to the array
+              foot_pos_deltas.foot_pos_deltas[i] = transform;
+            }
+            foot_pos_deltas.foot_pos_deltas[goal->leg].translation.x = goal->x_target;
+            foot_pos_deltas.foot_pos_deltas[goal->leg].translation.y = goal->y_target;
+            foot_pos_deltas.foot_pos_deltas[goal->leg].translation.z = goal->z_target;       
+            foot_pos_deltas_publisher_->publish(foot_pos_deltas);
+
+            auto result = std::make_shared<Leg_move_actions::Result>();
+            result->success = true;
+            goal_handle->succeed(result);
+            RCLCPP_INFO(this->get_logger(), "Goal execution completed");
+        }).detach();
+    }
 
     void rollCallback_(const std_msgs::msg::Float32 msg) {   
        imu_roll=msg.data;          
